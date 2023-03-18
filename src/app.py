12c5763e88e1bd25b0ca63e4884de3af3ac95ca0
Jonah Hamilton <jonah.hamilton@gmail.com>
2023-03-18 16:38:41 -0700

    switch to px bar chart

--- src/app.py
+++ src/app.py
@@ -1,11 +1,11 @@
 import os 
 
-from dash import dash, html, dcc, Input, Output
+from dash import dash, html, dcc, Input, Output, dash_table
+import plotly.express as px
 import altair as alt
 import pandas as pd
 import numpy as np
-
-# import dash_bootstrap_components as dbc
+import dash_bootstrap_components as dbc
 
 
 # load data
@@ -38,17 +38,19 @@ genre_list = [
  'Western']
 
 
-# app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
-app = dash.Dash(
-    __name__, external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"]
-)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUX])
+# app = dash.Dash(
+#     __name__, external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+# )
 
 app.layout = html.Div(
     [
-        html.Iframe(
-            id="barchart",
-            style={"border-width": "0", "width": "100%", "height": "400px"},
-        ),
+        # html.Iframe(
+        #     id="barchart",
+        #     style={"border-width": "0", "width": "100%", "height": "400px"},
+        # ),
+        dcc.Graph(id='bar-chart',
+                  style={"border-width": "0", "width": "100%", "height": "400px"}),
         "Select Rating Range",
         dcc.RangeSlider(
             id="rating",
@@ -84,7 +86,8 @@ app.layout = html.Div(
 
 
 @app.callback(
-        Output("barchart", "srcDoc"), 
+        # Output("barchart", "srcDoc"), 
+        Output('bar-chart', 'figure'),
         [Input("selected_genre", "value"),
         Input("rating", "value"),
         Input("runtime", "value")]
@@ -106,7 +109,6 @@ def plot_altair(selected_genre, rating, runtime):
                         )     
 
     else:
-        print(selected_genre, type(selected_genre), movies.columns, type(movies["genres"][0]))
         filtered_movies = (
                     movies
                     .query(
@@ -115,43 +117,51 @@ def plot_altair(selected_genre, rating, runtime):
                         "runtime <= @runtime[1] & "
                         "runtime >= @runtime[0] & revenue > 0"
                     )
-                    .loc[movies['genre_list'].str.contains(selected_genre),:]
+                    .loc[movies['genres'].str.contains(selected_genre),:]
                     .sort_values(["vote_average", "vote_count"], ascending=False)
                     .head(10)
                     )
 
-   
-    tooltips = [
-        alt.Tooltip("overview", title="Synopsis"),
-        alt.Tooltip("runtime", title="Runtime (mins)"),
-        alt.Tooltip("revenue", title="Gross Revenue (USD)"),
-        alt.Tooltip("vote_count", title="Votes"),
-        alt.Tooltip("genre_list", title="Genres"),
+
+    tooltips = [filtered_movies['overview'], filtered_movies['runtime'],
+                filtered_movies['revenue'], filtered_movies['genre_list'],
+                filtered_movies['vote_count']
     ]
 
-    bar_chart = (
-        alt.Chart(filtered_movies)
-        .mark_bar()
-        .encode(
-            x=alt.X(
-                "vote_count",
-                title="Average Number of Votes",
-                scale=alt.Scale(zero=False),
-            ),
-            y=alt.Y(
-                "title",
-                sort="-x",
-                title="Movie Title",
-            ),
-            color=alt.Color(
-                "vote_average",
-                title="Average Vote Score",
-            ),
-            tooltip=tooltips,
-        )
-    )
+    hovertemp = ('Synopsis: %{tooltips[0]}<br>' +
+                               'Runtime (mins): %{tooltips[1]}<br>' +
+                               'Gross Revenue (USD): %{tooltips[2]:.2f}<br>' +
+                               'TGenres: %{tooltips[3]:.2f}<br>' +
+                               'Votes: %{tooltips[4]:.2f}<br>')
 
-    return bar_chart.to_html()
+
+
+    fig = px.bar(filtered_movies, x="vote_count", y='title', color='vote_average',
+                 title= "Top 10 Rated Movies",
+                 labels= {"vote_count" : "Average Number of Votes",
+                          "title" : "Movie Title",
+                          "vote_average" : "Average Vote Score" },
+                # barmode= "group",
+                custom_data=tooltips
+                          )
+    fig.update_layout(barmode='group', xaxis={'categoryorder':'total ascending'})
+    # Set the custom hover text labels
+
+    fig.update_traces(hovertemplate=hovertemp)
+    # fig.update_layout(yaxis=dict(autorange="reversed"))
+    
+
+        # Set the plot layout
+    # fig.update_layout(
+    #     margin=dict(l=20, r=20, t=30, b=20),
+    #     xaxis_title='Fruit',
+    #     yaxis_title='Count',
+    #     font=dict(size=12)
+    # )
+
+
+
+    return fig
 
 
 if __name__ == "__main__":
